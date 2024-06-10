53c579284756cff163cf111bfa55d9315eed6ec4
roninnn1 <roninnn1@yandex.ru>
2024-06-10 17:21:01 +0300

    1.4 Валидация данных

--- app/main.py
+++ app/main.py
@@ -1,7 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from datetime import date
+from pydantic import BaseModel
 
-app = FastAPI()     #   вызываю класс fastapi
+app = FastAPI()  # вызываю класс fastapi
 
-@app.get('/hotels')
-def get_hotels():
-    return "Какой то отель"
+
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
+
+@app.get('/hotels', response_model=list[SHotel])
+def get_hotels(
+    location: str,
+    date_from: date,
+    date_to: date,
+    has_spa: bool = Query(default=None),
+    stars: int = Query(ge=1, le=5, default=None)  # >= 1 и <= 5
+    ):
+    hotels = [
+        {
+            "address": 'Какойто адрес',
+            "name": 'Super Hotel',
+            "stars": 5
+        }
+    ]
+    return date_from, date_to
+
+
+class SBooking(BaseModel):  # поля для POST запроса
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+def add_booking(booking: SBooking):
+    pass
