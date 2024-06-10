18d6c3be0dfb416df2f5a43836326cc3d0b41f50
Esaul1391 <1391esaul@gmail.com>
2024-06-10 18:44:51 +0300

    First commit, create app.

--- app/main.py
+++ app/main.py
@@ -1,33 +1,37 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from datetime import date
 from pydantic import BaseModel
 
 app = FastAPI()  # вызываю класс fastapi
 
 
+class HotelsSearchArgs:
+    def __init__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            has_spa: bool = Query(default=None),
+            stars: int = Query(ge=1, le=5, default=None)  # >= 1 и <= 5
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+@app.get('/hotels')
+def get_hotels(
+        search_args: HotelsSearchArgs = Depends()
+):
+    return search_args
+
+
 class SHotel(BaseModel):
     address: str
     name: str
     stars: int
-
-
-
-@app.get('/hotels', response_model=list[SHotel])
-def get_hotels(
-    location: str,
-    date_from: date,
-    date_to: date,
-    has_spa: bool = Query(default=None),
-    stars: int = Query(ge=1, le=5, default=None)  # >= 1 и <= 5
-    ):
-    hotels = [
-        {
-            "address": 'Какойто адрес',
-            "name": 'Super Hotel',
-            "stars": 5
-        }
-    ]
-    return hotels
 
 
 class SBooking(BaseModel):  # поля для POST запроса
