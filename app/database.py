2f041255f3a1c83c4fdc909dbbe933d40539f03f
roninnn1 <roninnn1@yandex.ru>
2024-06-13 15:39:13 +0300

    1.4 Валидация данных

--- app/database.py
+++ app/database.py
@@ -1,19 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DB_HOST = 'localhost'
-DB_PORT = 5432
-DB_USER = 'postgres'
-DB_PASS = 'postgres'
-DB_NAME = 'postgres'
+from config import settings
 
 # Генерирую URL базы данных
 # asyncpg выполняет запросы к базе асинхронно
 # Асинхронные запросы к базе данных позволяют во время ожидания ответа от БД обрабатывать другие запросы
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # передаю в алхимию URL
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(settings.DATABASE_URL)
 
 # создаю генератор сессий, это транзакции к базе данных
 # транзакция это набор инструкций которы мы посылаем в базу данных
